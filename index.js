60481575454823965255698f7a1ab6bc1acbef0c
unknown <pbernier@globalia.ca>
2024-11-15 12:58:29 -0500

    Update homepage template and add API GET request

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
-    res.render('homepage',{ title: 'Homepage' });
+    const pets = 'https://api.hubspot.com/crm/v3/objects/pets?properties=name%2Cspecie%2Crace';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(pets, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Homepage', data });      
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
